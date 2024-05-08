37b041637d9ef8dfa9ba980b2faafbacfed92113
Julian Karl Bauer <juliankarlbauer @gmx.de>
2024-05-08 16:43:38 +0200

    Fix rename

--- scripts/s001_run.py
+++ scripts/s001_run.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pymetis
 
-manager = pymetis.Manager(path_config_file="./pymetis/example_files/pendulum.yml")
+manager = pymetis.Manager(path_config_file="./pymetis/example_files/pendulum2d.yml")
 result = manager.manage()
 
 
