off-by-one error fixed

pymetis/time_steppers.py
@@ -100,7 +100,7 @@ class FixedIncrement(TimeStepper):
         return np.linspace(
             start=self.start,
             stop=self.end,
-            num=self.number_of_steps,
+            num=self.number_of_steps+1,
             endpoint=True,
             dtype=np.float64,
         )

scripts/s005_time_stepper_fixed_step_size.py
@@ -1,10 +1,17 @@
 import numpy as np
 from pymetis import time_steppers
 
-stepper = time_steppers.FixedIncrementKinon(
+# stepper = time_steppers.FixedIncrementKinon(
+#     start=1.0,
+#     end=2.2,
+#     step_size=0.15,
+#     manager=None,
+# )
+
+stepper = time_steppers.FixedIncrement(
     start=1.0,
     end=2.2,
-    step_size=0.15,
+    number_of_steps=12,
     manager=None,
 )
 
@@ -15,7 +22,7 @@ from itertools import pairwise
 increments = np.array([n1 - n for n, n1 in pairwise(stepper.times)])
 
 increments_equal_step_size = np.isclose(increments, stepper.step_size)
-
+print(increments)
 print(increments_equal_step_size)
 
 assert all(