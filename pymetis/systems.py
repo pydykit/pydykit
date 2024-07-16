e5046dc04c7ac87cfdc010dec12ca6f5082bcf4f
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-07-16 23:19:57 +0200

    Comment out

--- pymetis/systems.py
+++ pymetis/systems.py
@@ -197,76 +197,88 @@ class Pendulum3DCartesian(MultiBodySystem):
 class PendulumQuaternions(MultiBodySystem):
 
     def initialize(self):
-        self.length = np.linalg.norm(self.initial_state["Q"])
-        self.ext_acc = np.array(self.ext_acc)
+        pass
+        # self.length = np.linalg.norm(self.initial_state["Q"])
+        # self.ext_acc = np.array(self.ext_acc)
 
-        self.states = states.State(
-            nbr_states=self.manager.time_stepper.nbr_time_points,
-            dim_state=2 * self.nbr_spatial_dimensions + self.nbr_constraints,
-            columns=[
-                "x",
-                "y",
-                "z",
-                "dx",
-                "dy",
-                "dz",
-                "lambda",
-            ],  # TODO: As the integrator defines whether it is velocity or momentum, this definition should be moved to integrator?
-        )
+        # self.states = states.State(
+        #     nbr_states=self.manager.time_stepper.nbr_time_points,
+        #     dim_state=2 * self.nbr_spatial_dimensions + self.nbr_constraints,
+        #     columns=[
+        #         "x",
+        #         "y",
+        #         "z",
+        #         "dx",
+        #         "dy",
+        #         "dz",
+        #         "lambda",
+        #     ],  # TODO: As the integrator defines whether it is velocity or momentum, this definition should be moved to integrator?
+        # )
 
-        self.states.state_n = self.states.state_n1 = self.states.state[0, :] = (
-            self.compose_state(
-                q=np.array(self.initial_state["Q"]),
-                p=self.get_mass_matrix(q=None) @ np.array(self.initial_state["V"]),
-                lambd=np.zeros(self.nbr_constraints),
-            )
-        )
+        # self.states.state_n = self.states.state_n1 = self.states.state[0, :] = (
+        #     self.compose_state(
+        #         q=np.array(self.initial_state["Q"]),
+        #         p=self.get_mass_matrix(q=None) @ np.array(self.initial_state["V"]),
+        #         lambd=np.zeros(self.nbr_constraints),
+        #     )
+        # )
 
     def decompose_state(self, state):
-        dim = self.nbr_spatial_dimensions
+        pass
+        # dim = self.nbr_spatial_dimensions
 
-        assert len(state) == 2 * dim + self.nbr_constraints
+        # assert len(state) == 2 * dim + self.nbr_constraints
 
-        decomposed_state = namedtuple("state", "q p lambd")
-        return decomposed_state(
-            q=state[0:dim],
-            p=state[dim : 2 * dim],
-            lambd=state[2 * dim :],
-        )
+        # decomposed_state = namedtuple("state", "q p lambd")
+        # return decomposed_state(
+        #     q=state[0:dim],
+        #     p=state[dim : 2 * dim],
+        #     lambd=state[2 * dim :],
+        # )
 
     def compose_state(self, q, p, lambd):
-        return np.concatenate(
-            [
-                q,
-                p,
-                lambd,
-            ],
-            axis=0,
-        )
+        pass
+        # return np.concatenate(
+        #     [
+        #         q,
+        #         p,
+        #         lambd,
+        #     ],
+        #     axis=0,
+        # )
 
     def get_mass_matrix(self, q):
-        return self.mass * np.eye(self.nbr_spatial_dimensions)
+        pass
+        # return self.mass * np.eye(self.nbr_spatial_dimensions)
 
     def kinetic_energy_gradient_from_momentum(self, q, p):
-        return np.zeros(q.shape)
+        pass
+        # return np.zeros(q.shape)
 
     def kinetic_energy_gradient_from_velocity(self, q, v):
-        return np.zeros(q.shape)
+        pass
+        # return np.zeros(q.shape)
 
     def external_potential(self, q):
-        return -(self.get_mass_matrix(q=q) @ self.ext_acc).T @ q
+        pass
+        # return -(self.get_mass_matrix(q=q) @ self.ext_acc).T @ q
 
     def external_potential_gradient(self, q):
-        return -self.get_mass_matrix(q=q) @ self.ext_acc
+        pass
+        # return -self.get_mass_matrix(q=q) @ self.ext_acc
 
     def internal_potential(self):
-        return 0.0
+        pass
+        # return 0.0
 
     def internal_potential_gradient(self, q):
-        return np.zeros(q.shape)
+        pass
+        # return np.zeros(q.shape)
 
     def constraint(self, q):
-        return 0.5 * (q.T @ q / self.length**2 - 1.0)
+        pass
+        # return 0.5 * (q.T @ q / self.length**2 - 1.0)
 
     def constraint_gradient(self, q):
-        return q.T[np.newaxis, :] / self.length**2
+        pass
+        # return q.T[np.newaxis, :] / self.length**2
