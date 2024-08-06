f7e5d983f517d37e9acc4b8e770abc4f1cdb9b1c
Philipp Kinon <philipp.kinon@kit.edu>
2024-08-06 18:28:36 +0200

    consistent naming

--- scripts/s008_create_phmbs.py
+++ scripts/s008_create_phmbs.py
@@ -1,6 +1,6 @@
 import pydykit
 
 manager = pydykit.Manager(
-    path_config_file="./pydykit/example_files/porthamiltonian_fourparticle_system.yml"
+    path_config_file="./pydykit/example_files/porthamiltonianfourparticlesystem.yml"
 )
 manager.system.initialize()
