bd976861a73e459caac1ecc3fc48d3e69f728319
Philipp Kinon <philipp.kinon@kit.edu>
2024-08-11 16:04:19 +0200

    colorblind-friendly palette

--- scripts/s009_graphics.py
+++ scripts/s009_graphics.py
@@ -15,7 +15,25 @@ print("Success, start plotting")
 
 df = result.to_df()
 
+# color scheme (color-blind friendly)
+# https://clauswilke.com/dataviz/color-pitfalls.html#not-designing-for-color-vision-deficiency
+my_color_palette = [
+    "#0072B2",
+    "#009E73",
+    "#D55E00",
+    "#56B4E9",
+    "#CC79A7",
+    "#E69F00",
+    "#F0E442",
+]
 
 # Export using tikzplotlib, see https://pypi.org/project/tikzplotly/
-fig = px.line(df, x="x", y="y", markers=True, labels={"x": "$x$", "y": "$y$"})
-tikzplotly.save("docs/tex_export/example.tex", fig)
+fig = px.line(
+    df,
+    x="x",
+    y="y",
+    markers=True,
+    labels={"x": "$x$", "y": "$y$"},
+    color_discrete_sequence=my_color_palette,
+)
+tikzplotly.save("docs/tex_export/example_figure.tex", fig)
