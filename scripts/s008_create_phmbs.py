c08e6f81963a7e8c72ade18cd3a501e433b0327e
Philipp Kinon <philipp.kinon@kit.edu>
2024-08-16 15:46:21 +0200

    creat PHS from MBS and compute with MBS integrator

--- scripts/s008_create_phmbs.py
+++ scripts/s008_create_phmbs.py
@@ -1,6 +1,105 @@
 import pydykit
+import pydykit.systems
+import plotly.graph_objects as go
+
 
 manager = pydykit.Manager(
     path_config_file="./pydykit/example_files/porthamiltonianfourparticlesystem.yml"
 )
-manager.system.initialize()
+manager.system.initialize()  # creates MBS named FourParticleSystem
+
+PHS = pydykit.systems.PortHamiltonianMBS(manager=manager)
+PHS.initialize(MultiBodySystem=manager.system)
+# creates an instance of PHS with attribute MBS
+manager.system = PHS
+
+result = manager.manage()
+
+
+df = result.to_df()
+
+fig = go.Figure(
+    data=go.Scatter3d(
+        x=df["x1"],
+        y=df["y1"],
+        z=df["z1"],
+        marker=dict(
+            size=3,
+            color=df["time"],
+            colorscale="Viridis",
+            colorbar=dict(
+                thickness=20,
+                title="time",
+            ),
+        ),
+        line=dict(
+            color="darkblue",
+            width=3,
+        ),
+    )
+)
+
+fig.add_trace(
+    go.Scatter3d(
+        x=df["x2"],
+        y=df["y2"],
+        z=df["z2"],
+        marker=dict(
+            size=3,
+            color=df["time"],
+            colorscale="Viridis",
+            colorbar=dict(
+                thickness=20,
+                title="time",
+            ),
+        ),
+        line=dict(
+            color="darkblue",
+            width=3,
+        ),
+    )
+)
+
+fig.add_trace(
+    go.Scatter3d(
+        x=df["x3"],
+        y=df["y3"],
+        z=df["z3"],
+        marker=dict(
+            size=3,
+            color=df["time"],
+            colorscale="Viridis",
+            colorbar=dict(
+                thickness=20,
+                title="time",
+            ),
+        ),
+        line=dict(
+            color="darkblue",
+            width=3,
+        ),
+    )
+)
+
+fig.add_trace(
+    go.Scatter3d(
+        x=df["x4"],
+        y=df["y4"],
+        z=df["z4"],
+        marker=dict(
+            size=3,
+            color=df["time"],
+            colorscale="Viridis",
+            colorbar=dict(
+                thickness=20,
+                title="time",
+            ),
+        ),
+        line=dict(
+            color="darkblue",
+            width=3,
+        ),
+    )
+)
+
+fig.show()
