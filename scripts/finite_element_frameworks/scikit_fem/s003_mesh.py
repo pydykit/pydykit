8580119231db04b5667b8115b8da28738cd4cd71
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-09-08 23:19:52 +0200

    Follow https://scikit-fem.readthedocs.io/en/latest/howto.html#visualizing-the-solution

--- scripts/finite_element_frameworks/scikit_fem/s003_mesh.py
+++ scripts/finite_element_frameworks/scikit_fem/s003_mesh.py
@@ -1,11 +1,11 @@
 import numpy as np
 import skfem
 import utils
-from skfem.visuals import matplotlib as skfem_mpl
+from skfem.visuals.matplotlib import plot
 
 mesh = skfem.MeshTri().refined(3)
 
-ax = skfem_mpl.plot(
+ax = plot(
     mesh,
     np.ones(mesh.nelements),
 )
@@ -13,14 +13,14 @@ utils.dump_plt(name="ones_elements")
 
 random_elements = np.random.rand(mesh.nelements)
 
-ax = skfem_mpl.plot(
+ax = plot(
     mesh,
     random_elements,
 )
 utils.dump_plt(name="random_elements")
 
 
-ax = skfem_mpl.plot(
+ax = plot(
     mesh,
     random_elements[0 : mesh.nvertices],
 )
