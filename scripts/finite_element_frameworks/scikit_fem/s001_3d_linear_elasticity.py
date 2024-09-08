93e20841adf8cc7f6b7fb751efd0dff9b17f520c
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-09-08 23:16:25 +0200

    Format

--- scripts/finite_element_frameworks/scikit_fem/s001_3d_linear_elasticity.py
+++ scripts/finite_element_frameworks/scikit_fem/s001_3d_linear_elasticity.py
@@ -10,19 +10,32 @@ from skfem.models.elasticity import lame_parameters
 
 m = skfem.MeshHex().refined(3).with_defaults()
 e = skfem.ElementVector(skfem.ElementHex1())
-basis = skfem.Basis(m, e, intorder=3)
+basis = skfem.Basis(
+    m,
+    e,
+    intorder=3,
+)
 
 # calculate Lamé parameters from Young's modulus and Poisson ratio
-lam, mu = lame_parameters(1e3, 0.3)
+lam, mu = lame_parameters(
+    E=1e3,
+    nu=0.3,
+)
 
 
 def C(T):
-    return 2.0 * mu * T + lam * eye(trace(T), T.shape[0])
+    return 2.0 * mu * T + lam * eye(
+        trace(T),
+        T.shape[0],
+    )
 
 
 @skfem.BilinearForm
 def stiffness(u, v, w):
-    return ddot(C(sym_grad(u)), sym_grad(v))
+    return ddot(
+        C(sym_grad(u)),
+        sym_grad(v),
+    )
 
 
 K = stiffness.assemble(basis)
@@ -30,10 +43,18 @@ K = stiffness.assemble(basis)
 u = basis.zeros()
 u[basis.get_dofs("right").nodal["u^1"]] = 0.3
 
-u = skfem.solve(*skfem.condense(K, x=u, D=basis.get_dofs({"left", "right"})))
+u = skfem.solve(
+    *skfem.condense(
+        K,
+        x=u,
+        D=basis.get_dofs({"left", "right"}),
+    ),
+)
 
 sf = 1.0
-m = m.translated(sf * u[basis.nodal_dofs])
+m = m.translated(
+    sf * u[basis.nodal_dofs],
+)
 
 if __name__ == "__main__":
     from os.path import splitext
