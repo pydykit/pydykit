b8b1d36138abef051c141ed3e6c5ea4bbb684a02
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-09-08 21:37:51 +0200

    Add reference

--- scripts/finite_element_frameworks/scikit_fem/s002_play_with_forms.py
+++ scripts/finite_element_frameworks/scikit_fem/s002_play_with_forms.py
@@ -22,3 +22,4 @@ skfem.asm(
 # your started a Python debugger at the breakpoint in line 9
 #
 # Therefore, you are able to inspect the local namespace at line 9 during execution.
+# See also https://scikit-fem.readthedocs.io/en/latest/advanced.html#anatomy-of-forms
