5a9576540489d0fa5475e3e13e234a335da37335
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-09-08 23:03:05 +0200

    Try to get the difference between plotting nodes or elements

--- scripts/finite_element_frameworks/scikit_fem/s003_mesh.py
+++ scripts/finite_element_frameworks/scikit_fem/s003_mesh.py
@@ -5,9 +5,23 @@ from skfem.visuals import matplotlib as skfem_mpl
 
 mesh = skfem.MeshTri().refined(3)
 
-ax = skfem_mpl.plot(mesh, np.random.rand(mesh.nelements))
+ax = skfem_mpl.plot(
+    mesh,
+    np.ones(mesh.nelements),
+)
+utils.dump_plt(name="ones_elements")
 
-utils.dump_plt(name="random")
+random_elements = np.random.rand(mesh.nelements)
 
-ax = skfem_mpl.plot(mesh, np.ones(mesh.nelements))
-utils.dump_plt(name="ones")
+ax = skfem_mpl.plot(
+    mesh,
+    random_elements,
+)
+utils.dump_plt(name="random_elements")
+
+
+ax = skfem_mpl.plot(
+    mesh,
+    random_elements[0 : mesh.nvertices],
+)
+utils.dump_plt(name="random_vertices")
