f33c010fd547992f652bb7541f14d5063c8c769c
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-10-27 11:32:28 +0100

    Validate integrator class_name

--- pydykit/configuration.py
+++ pydykit/configuration.py
@@ -1,4 +1,8 @@
-from pydantic import BaseModel
+from enum import Enum
+
+from pydantic import BaseModel, field_validator
+
+from .factories import integrator_factory
 
 
 class ClassNameKwargs(BaseModel):
@@ -6,8 +10,18 @@ class ClassNameKwargs(BaseModel):
     kwargs: dict | None
 
 
+class Integrator(ClassNameKwargs):
+
+    @field_validator("class_name")
+    def validator(cls, arg):
+        options = integrator_factory.constructors
+        if arg not in options:
+            raise ValueError(f"supported options are {options}")
+        return arg
+
+
 class Configuration(BaseModel):
     system: ClassNameKwargs
     simulator: ClassNameKwargs
-    integrator: ClassNameKwargs
+    integrator: Integrator
     time_stepper: ClassNameKwargs
