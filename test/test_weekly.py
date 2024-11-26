422d357124bbb0d25d0b15bd74d8ddcb7e0a159e
Philipp Kinon <philipp.kinon@kit.edu>
2024-11-26 14:38:21 +0100

    only run scripts and dont assert results

--- test/test_weekly.py
+++ test/test_weekly.py
@@ -1,64 +1,42 @@
-import numpy as np
+from pathlib import Path
+
 import pytest
 
-import pydykit
-import pydykit.examples
-import pydykit.postprocessors as postprocessors
-import pydykit.systems_port_hamiltonian as phs
+from .constants import PATH_PUBLICATIONS_DIRECTORY
 
-from . import utils
-from .constants import A_TOL, PATH_REFERENCE_RESULTS, R_TOL
+projects = ["2024_kinon_morandin_schulze_XYZ"]
 
-example_manager = pydykit.examples.Manager()
 
-folders = ["../publications/"]
+def get_python_files(directory):
+    # Use Path.glob to find all .py files in the directory
+    python_files = [str(file.absolute().stem) for file in Path(directory).glob("*.py")]
+    return python_files
 
-scripts = ["2024_kinon_morandin_schulze_XYZ"]
+
+def run_script_code(script_name):
+    with open(script_name) as file:
+        script_content = file.read()
+    exec(script_content)
 
 
 class TestExamples:
     @pytest.mark.weekly
     @pytest.mark.parametrize(
-        ("content_config_file", "expected_result_df"),
+        ("publication_scripts", "project"),
         (
             pytest.param(
-                example_manager.get_example(name=key),
-                utils.load_result_of_pydykit_simulation(
-                    path=PATH_REFERENCE_RESULTS.joinpath(f"{location}/{key}.csv")
+                get_python_files(
+                    directory=PATH_PUBLICATIONS_DIRECTORY.joinpath(f"{key}")
                 ),
+                key,
                 id=key,
             )
-            for location, key in zip(folders, scripts)
+            for key in projects
         ),
     )
-    def test_run_examples(self, content_config_file, expected_result_df):
+    def test_run_publication_scripts(self, publication_scripts, project):
 
-        manager = pydykit.managers.Manager()
-        configuration = pydykit.configuration.Configuration(
-            **content_config_file["configuration"],
-        )
-        manager._configure(configuration=configuration)
-
-        # intermediate steps if conversion to PH system is necessary
-        porthamiltonian_system = phs.PortHamiltonianMBS(manager=manager)
-        # creates an instance of PHS with attribute MBS
-        manager.system = porthamiltonian_system
-
-        result = pydykit.results.Result(manager=manager)
-        result = manager.manage(result=result)
-        new = result.to_df()
-
-        postprocessor = postprocessors.Postprocessor(manager, state_results_df=new)
-        postprocessor.postprocess(quantities=["hamiltonian"], evaluation_points=["n"])
-
-        old = expected_result_df
-        new = postprocessor.results_df
-
-        utils.print_compare(old=old, new=new)
-
-        assert np.allclose(
-            old,
-            new,
-            rtol=R_TOL,
-            atol=A_TOL,
-        )
+        for script in publication_scripts:
+            run_script_code(
+                PATH_PUBLICATIONS_DIRECTORY.joinpath(f"{project}/{script}.py")
+            )
