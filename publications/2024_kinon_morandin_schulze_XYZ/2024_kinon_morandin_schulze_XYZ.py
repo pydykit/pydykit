72bb08964788565906b55ceccbbea6045c94acb0
Philipp Kinon <philipp.kinon@kit.edu>
2024-11-26 11:22:18 +0100

    first steps to publication tests on weekly basis

--- publications/2024_kinon_morandin_schulze_XYZ/2024_kinon_morandin_schulze_XYZ.py
+++ publications/2024_kinon_morandin_schulze_XYZ/2024_kinon_morandin_schulze_XYZ.py
@@ -5,59 +5,60 @@ import pydykit.systems_port_hamiltonian as phs
 project = "2024_kinon_morandin_schulze_XYZ"
 name = "four_particle_system"
 
-# manager = pydykit.managers.Manager()
+manager = pydykit.managers.Manager()
 
-# path_config_file = f"./publications/{project}/{name}.yml"
+path_config_file = f"./publications/{project}/{name}.yml"
 
-# manager.configure_from_path(path=path_config_file)
+manager.configure_from_path(path=path_config_file)
 
-# porthamiltonian_system = phs.PortHamiltonianMBS(manager=manager)
-# # creates an instance of PHS with attribute MBS
-# manager.system = porthamiltonian_system
+porthamiltonian_system = phs.PortHamiltonianMBS(manager=manager)
+# creates an instance of PHS with attribute MBS
+manager.system = porthamiltonian_system
 
-# result = pydykit.results.Result(manager=manager)
-# result = manager.manage(result=result)
+result = pydykit.results.Result(manager=manager)
+result = manager.manage(result=result)
 
-# df = result.to_df()
-# postprocessor = postprocessors.Postprocessor(manager, state_results_df=df)
-# postprocessor.postprocess(
-#     quantities=["hamiltonian", "hamiltonian", "constraint", "constraint_velocity"],
-#     evaluation_points=["n", "n1-n", "n", "n"],
-# )
+df = result.to_df()
+postprocessor = postprocessors.Postprocessor(manager, state_results_df=df)
+postprocessor.postprocess(
+    quantities=["hamiltonian", "hamiltonian", "constraint", "constraint_velocity"],
+    evaluation_points=["n", "n1-n", "n", "n"],
+)
 
-# postprocessor.postprocess(
-#     quantities=["dissipated_power"],
-#     evaluation_points=["n05"],
-#     weighted_by_timestepsize=True,
-# )
-# postprocessor.add_sum_of(
-#     quantities=["hamiltonian_difference", "dissipated_power"], name="sum"
-# )
+postprocessor.postprocess(
+    quantities=["dissipated_power"],
+    evaluation_points=["n05"],
+    weighted_by_timestepsize=True,
+)
+postprocessor.add_sum_of(
+    quantities=["hamiltonian_difference", "dissipated_power"], name="sum"
+)
 
-# # Hamiltonian
-# fig01 = postprocessor.visualize(quantities=["hamiltonian"])
-# fig01.show()
+# Hamiltonian
+fig01 = postprocessor.visualize(quantities=["hamiltonian"])
+fig01.show()
 
-# postprocessor.results_df["sum"] = abs(postprocessor.results_df["sum"])
+postprocessor.results_df["sum"] = abs(postprocessor.results_df["sum"])
 
-# fig02 = postprocessor.visualize(
-#     quantities=["hamiltonian_difference", "dissipated_power", "sum"],
-# )
-# fig02.show()
+fig02 = postprocessor.visualize(
+    quantities=["hamiltonian_difference", "dissipated_power", "sum"],
+)
+fig02.show()
 
-# fig03 = postprocessor.visualize(quantities=["sum"], y_axis_scale="log")
-# fig03.show()
+fig03 = postprocessor.visualize(quantities=["sum"], y_axis_scale="log")
+fig03.show()
 
-# fig03 = postprocessor.visualize(quantities=["constraint"], y_axis_label="constraints")
-# fig03.show()
+fig03 = postprocessor.visualize(quantities=["constraint"], y_axis_label="constraints")
+fig03.show()
 
-# fig04 = postprocessor.visualize(
-#     quantities=["constraint_velocity"], y_axis_label="velocity constraints"
-# )
-# fig04.show()
+fig04 = postprocessor.visualize(
+    quantities=["constraint_velocity"], y_axis_label="velocity constraints"
+)
+fig04.show()
 
 # postprocessor.results_df.to_csv(f"./publications/{project}/{name}.csv", index=False)
 
+#####
 
 # name = "four_particle_system_MP"
 
@@ -139,33 +140,33 @@ name = "four_particle_system"
 ########
 
 
-name = "four_particle_system_blowup_DG"
+# name = "four_particle_system_blowup_DG"
 
-manager_4 = pydykit.managers.Manager()
+# manager_4 = pydykit.managers.Manager()
 
-path_config_file_4 = f"./publications/{project}/{name}.yml"
+# path_config_file_4 = f"./publications/{project}/{name}.yml"
 
-manager_4.configure_from_path(path=path_config_file_4)
+# manager_4.configure_from_path(path=path_config_file_4)
 
-porthamiltonian_system_4 = phs.PortHamiltonianMBS(manager=manager_4)
-# creates an instance of PHS with attribute MBS
-manager_4.system = porthamiltonian_system_4
+# porthamiltonian_system_4 = phs.PortHamiltonianMBS(manager=manager_4)
+# # creates an instance of PHS with attribute MBS
+# manager_4.system = porthamiltonian_system_4
 
-result_4 = pydykit.results.Result(manager=manager_4)
-result_4 = manager_4.manage(result=result_4)
+# result_4 = pydykit.results.Result(manager=manager_4)
+# result_4 = manager_4.manage(result=result_4)
 
-df_4 = result_4.to_df()
-postprocessor_4 = postprocessors.Postprocessor(manager_4, state_results_df=df_4)
-postprocessor_4.postprocess(
-    quantities=["hamiltonian"],
-    evaluation_points=["n"],
-)
+# df_4 = result_4.to_df()
+# postprocessor_4 = postprocessors.Postprocessor(manager_4, state_results_df=df_4)
+# postprocessor_4.postprocess(
+#     quantities=["hamiltonian"],
+#     evaluation_points=["n"],
+# )
 
-# Hamiltonian
-fig01 = postprocessor_4.visualize(quantities=["hamiltonian"])
-fig01.show()
+# # Hamiltonian
+# fig01 = postprocessor_4.visualize(quantities=["hamiltonian"])
+# fig01.show()
 
-postprocessor_4.results_df.to_csv(f"./publications/{project}/{name}.csv", index=False)
+# postprocessor_4.results_df.to_csv(f"./publications/{project}/{name}.csv", index=False)
 
 ######
 
