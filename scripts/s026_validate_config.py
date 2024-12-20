a614e2915dd7f6f4c16aaea2e28731c0047d3df3
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-12-20 18:15:46 +0100

    Start with script

---
+++ scripts/s026_validate_config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from pydykit.configuration import Configuration
+from pydykit.utils import load_yaml_file
+
+path = Path(__file__).parent.parent.joinpath(
+    "pydykit", "example_files", "particle_system_02.yml"
+)
+
+file_content = load_yaml_file(
+    path=path,
+)
+
+configuration = Configuration(
+    **file_content["configuration"],
+)
