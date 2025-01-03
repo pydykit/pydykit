0d5db0d3a8fd499e4fed95dd845bb13305aafb4c
Philipp Kinon <philipp.kinon@kit.edu>
2025-01-03 21:50:04 +0100

    system testing obsolete

--- test/test_configuration.py
+++ test/test_configuration.py
@@ -1,14 +1,7 @@
 import pytest
 from pydantic import ValidationError
 
-from pydykit.configuration import Integrator, System
-
-
-class TestConfiguration:
-    def test_invalid_class_name(self):
-        with pytest.raises(ValidationError) as excinfo:
-            System(class_name="my_class")
-        assert "Input should be" in str(excinfo.value)
+from pydykit.configuration import Integrator
 
 
 class TestIntegratorConfig:
