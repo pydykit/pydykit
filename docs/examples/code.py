3a36bb7cf14bd2f670a00e7e7a37df9fe563c6fd
Julian Karl Bauer <juliankarlbauer@gmx.de>
2025-01-12 21:16:30 +0100

    Derive examples and link

--- docs/examples/code.py
+++ docs/examples/code.py
@@ -2,6 +2,20 @@ from markdown import markdown
 
 from pydykit import examples
 
+
+def to_markdown_list(llist):
+    return "\n".join(
+        [
+            f"- [{item}](https://github.com/pydykit/pydykit/tree/main/pydykit/example_files/{item}.yml)"
+            for item in llist
+        ]
+    )
+
+
 keys = examples.ExampleManager().examples.keys()
-html = markdown(f"# greeting from python \n {keys}")
+html = markdown(
+    f"""
+{to_markdown_list(llist=keys)}
+"""
+)
 print(html)
