ca4a1276ac238797a14ddb01294059a24a361431
Julian Karl Bauer <juliankarlbauer@gmx.de>
2025-01-18 23:18:25 +0100

    Avoid copy paste

--- test/test_examples.py
+++ test/test_examples.py
@@ -5,14 +5,15 @@ import pydykit
 import pydykit.examples
 from pydykit.configuration import Configuration
 from pydykit.managers import Manager
-from pydykit.results import Result
+from pydykit.postprocessors import Postprocessor
+from pydykit.systems_port_hamiltonian import PortHamiltonianMBS
 
 from . import utils
 from .constants import A_TOL, PATH_REFERENCE_RESULTS, R_TOL
 
 example_manager = pydykit.examples.ExampleManager()
 
-example_worklist = [
+worklist = [
     "pendulum_3d",
     "pendulum_2d",
     "two_particle_system",
@@ -21,34 +22,51 @@ example_worklist = [
     "visco_pendulum",
     "lorenz",
     "reactor",
+    "four_particle_system_ph_discrete_gradient_dissipative",
 ]
+
+phmbs = ["four_particle_system_ph_discrete_gradient_dissipative"]
 
 
 class TestExamples:
     @pytest.mark.parametrize(
-        ("content_config_file", "expected_result_df"),
+        ("content_config_file", "expected_result_df", "is_phmbs"),
         (
             pytest.param(
                 example_manager.get_example(name=key),
                 utils.load_result_of_pydykit_simulation(
                     path=PATH_REFERENCE_RESULTS.joinpath(f"{key}.csv")
                 ),
+                key in phmbs,
                 id=key,
             )
-            for key in example_worklist
+            for key in worklist
         ),
     )
-    def test_run_examples(self, content_config_file, expected_result_df):
+    def test_run_examples(self, content_config_file, expected_result_df, is_phmbs):
 
         manager = Manager()
         configuration = Configuration(
             **content_config_file,
         )
         manager.configure(configuration=configuration)
-        result = manager.manage()
 
+        if is_phmbs:
+            # intermediate steps if conversion to PH system is necessary
+            porthamiltonian_system = PortHamiltonianMBS(manager=manager)
+            # creates an instance of PHS with attribute MBS
+            manager.system = porthamiltonian_system
+
+        result = manager.manage()
         old = expected_result_df
         new = result.to_df()
+
+        if is_phmbs:
+            postprocessor = Postprocessor(manager, state_results_df=new)
+            postprocessor.postprocess(
+                quantities_and_evaluation_points={"hamiltonian": ["current_time"]}
+            )
+            new = postprocessor.results_df
 
         utils.print_compare(old=old, new=new)
 
